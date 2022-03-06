2d2ca3bec3d92d1a8556b67e23e62a8d370c8aae
Robiyakhon <robkuch123@gmail.com>
2022-03-06 22:59:07 +0300

    Main class variable is added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi there for you all ... !");
-        System.out.println("Hello are you ok ...?");
-        System.out.println("Joke . . .?");
-        System.out.println("Nope it is not.");
+        int var = 10 ;
+        System.out.println(var);
 
 
     }
