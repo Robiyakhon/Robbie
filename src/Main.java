5024f9c50461ed61703d7b981c55fb1cc0c7b406
Robiyakhon <robkuch123@gmail.com>
2022-03-06 22:52:04 +0300

    Main.java modified v.02

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there for you all ... !");
+        System.out.println("Hello are you ok ...?");
+        System.out.println("Joke . . .?");
+        System.out.println("Nope it is not.");
+
+
     }
 }
